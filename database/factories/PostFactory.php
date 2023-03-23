2aafa1d83c46ef13e80fb85b27b4b98e97ce1011
Maxim <maxshelkous@gmail.com>
2023-03-23 12:08:20 +0200

    added DatabaseSeeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->unique()->word,
+            'content' => fake()->unique()->text,
+            'category_id' => Category::get()->random(),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return static
+     */
+    public function unverified()
+    {
+
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+//         \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin',
+            'role' => 0,
+            'password' => '$2y$10$iKNZ3UMikAwFr/aDe9d8hObblDTd0b2inY5nimhDKwav5.gRkOzkK', //adminadmin
+        ]);
+
+        Category::factory()->create([
+
+            'title' => 'Без категорії',
+
+        ]);
+        Category::factory( 5)->create();
+        Post::factory(5)->create();
     }
 }
